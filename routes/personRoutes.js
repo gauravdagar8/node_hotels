38e09f7d0fccd434df2ebae1af5976de10cbb820
gauravdagar8 <g47615756@gmail.com>
2024-06-23 17:25:00 +0530

    not change

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('././models/person.js');
+const Person = require('./../models/person.js');
 
 // POST route to add a person
 router.post('/', async(req, res) => {
