2d58c0970bbbf3c6a98a19011c692913983db76e
gauravdagar8 <g47615756@gmail.com>
2024-06-23 13:54:31 +0530

    shift mongodb to online server

--- first.js
+++ first.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 // console.log(bodyParser);
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res) {
@@ -21,6 +23,6 @@ const menuItemRoutes = require('./routes/MenuItemRoutes.js');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
   console.log("listening to the port 3000");
 })
